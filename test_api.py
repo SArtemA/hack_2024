cc71dcc9dd85a50679675e2c443c7caba8de66f8
SArtemA <148715539+SArtemA@users.noreply.github.com>
2024-07-07 02:20:09 +0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -12,7 +12,7 @@ def test_run_model():
     test_image_path = 'data'  # Путь тестовый
     files = {'path': test_image_path,}
     response = client.post("/run_model/", data=files)
-    print(response.content)
+    print(response)
     assert response.headers["content-type"] == 'application/json'
     assert response.status_code == 200
     

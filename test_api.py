85196c5e227d1fb93b9b7c84952e5aa0e2ec5e54
SArtemA <148715539+SArtemA@users.noreply.github.com>
2024-07-07 00:34:37 +0500

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,18 @@
+# Импорт библиотек
+import os
+import pytest
+from fastapi.testclient import TestClient
+from st_app import app
+
+# Создание клиента для тестирования
+client = TestClient(app)
+
+# Тестируем endpoint /run_model/ с изображением
+def test_run_model():
+    test_image_path = 'data'  # Путь тестовый
+    with open(test_image_path, "rb") as file:
+        files = {'path': file,}
+        response = client.post("/run_model/", data=files)
+    
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/csv"
